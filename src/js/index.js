58a74af4541cc6e90b25de91a736830b82c9fda0
georgepostoronca <george.postoronca@gmail.com>
2022-03-30 00:22:36 +0300

    carts

--- src/js/index.js
+++ src/js/index.js
@@ -396,27 +396,23 @@ if(headerHoverItem && headerHoverOpened) {
 }
 
 
-// const swiper = new Swiper(".mySwiper", {
-//     direction: "vertical",
-//     loop: true,
-//     spaceBetween: 7,
-//     slidesPerView: 5,
-//     freeMode: true,
-//     watchSlidesProgress: true,
-//     navigation: {
-//         nextEl: ".swiper-button-next",
-//         prevEl: ".swiper-button-prev",
-//     },
-// });
-//
-// const swiper2 = new Swiper(".mySwiper2", {
-//     loop: true,
-//     spaceBetween: 10,
-//     navigation: {
-//         nextEl: ".swiper-button-next",
-//         prevEl: ".swiper-button-prev",
-//     },
-//     thumbs: {
-//         swiper: swiper,
-//     },
-// });
+const swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 5,
+    freeMode: true,
+    watchSlidesProgress: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+const swiper2 = new Swiper(".mySwiper2", {
+    loop: true,
+    spaceBetween: 10,
+    thumbs: {
+        swiper: swiper,
+    },
+});
